4fb3236c81445373c236f4876217bf0e60b3bf32
Steve <stevekentlanier@gmail.com>
2021-06-06 13:59:10 -0500

    solved reverse-integer

--- Python/7.reverse-integer.py
+++ Python/7.reverse-integer.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        return int(str(x)[::-1])
+        if x < 0:
+            ans = int("-" + str(abs(x))[::-1])
+        else:
+            ans = int(str(abs(x))[::-1])
+        if abs(ans) > 2**31:
+            return 0
+        else:
+            return ans
 # @lc code=end
 
