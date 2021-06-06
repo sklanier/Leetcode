c4451cf1de202b4fdd672afe547a9296e2677933
Steve <stevekentlanier@gmail.com>
2021-06-06 13:45:34 -0500

    started reverse-integer

---
+++ Python/7.reverse-integer.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=7 lang=python3
+#
+# [7] Reverse Integer
+#
+
+# @lc code=start
+class Solution:
+    def reverse(self, x: int) -> int:
+        return int(str(x)[::-1])
+# @lc code=end
+
