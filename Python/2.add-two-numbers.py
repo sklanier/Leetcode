d8e77b9ac7fe6b7fad4e83d8267f9cc0b331888e
Steve <stevekentlanier@gmail.com>
2021-06-06 13:38:00 -0500

    attempt 1 - passing 2/1568 cases

--- Python/2.add-two-numbers.py
+++ Python/2.add-two-numbers.py
@@ -6,18 +6,36 @@
 
 # @lc code=start
 # Definition for singly-linked list.
+import math
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution:
+    def insert(root, item):
+        temp = ListNode(item)
+
+        if (root == None):
+            root = temp
+        else:
+            pointer = root
+            while (pointer.next != None):
+                pointer = pointer.next
+            pointer.next = temp
+        return root
+    def arrayToList(arr , n):
+        root = None
+        for i in range(0, n, 1):
+            root = Solution.insert(root, arr[i])
+        return root
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         cur1, cur2 = l1, l2
         num1 = []
         num2 = []
         out1 = ''
         out2 = ''
-        answer = []
+        l3 = None
         while cur1:
             num1.append(cur1.val)
             cur1 = cur1.next
@@ -30,9 +48,18 @@ class Solution:
             out2 += str(_)
         print(out1)
         print(out2)
-        final = str(int(out1) + int(out2))
-        for _ in final:
-            answer.append(int(_))
-        return answer
-# @lc code=end
+        final = []
+        
+        for i in range(len(out1)):
+            final.append(int(out1[i])+int(out2[i]))
+        for i in range(len(final)):
+            if final[i] > 9:
+                final[i] -= 10
+                final[i+1] += 1
+
+        print(final)
+        n = len(final)
+        l3 = Solution.arrayToList(final, n)
+        return l3
+ # @lc code=end
 
