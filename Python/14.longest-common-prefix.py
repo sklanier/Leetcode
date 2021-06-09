23987ca6b3b2a57c17cd283af24d4257d84ff9b2
Steve <stevekentlanier@gmail.com>
2021-06-09 13:47:38 -0500

    continuing work on longest common prefix

--- Python/14.longest-common-prefix.py
+++ Python/14.longest-common-prefix.py
@@ -8,6 +8,10 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         count = 0
-        
+        print(strs[0])
+        for i in range(len(strs)):
+            for j in range(len(strs[i])+1):
+                print(strs[i][:j])
+
 # @lc code=end
 
