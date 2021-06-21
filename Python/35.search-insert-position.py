894becb3fbe46586d1168d0438aac8aa55b693b9
Steve <stevekentlanier@gmail.com>
2021-06-21 13:08:00 -0500

    solved (kinda) searchInsert

--- Python/35.search-insert-position.py
+++ Python/35.search-insert-position.py
@@ -9,6 +9,20 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
             return nums.index(target)
-        
+        elif len(nums) < 2:
+            if target < nums[0]:
+                return 0
+            else:
+                return 1
+        else:
+            for i in range(len(nums) - 1):
+                if target > nums[i] and target < nums[i + 1]:
+                    return i + 1
+                elif target > nums[-1]:
+                    return len(nums)
+                elif target < nums[0]:
+                    return 0
+                else:
+                    continue
 # @lc code=end
 
