a2928ff25934ba20187a4135c4eff56a39caf638
Steve <stevekentlanier@gmail.com>
2021-06-22 13:01:50 -0500

    solved add binary

---
+++ Python/67.add-binary.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=67 lang=python3
+#
+# [67] Add Binary
+#
+
+# @lc code=start
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a, 2) + int(b, 2)).replace('0b', '')
+        
+# @lc code=end
+
