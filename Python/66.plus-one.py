3fcecf2087146d169801499ebd632124c01b39fc
Steve <stevekentlanier@gmail.com>
2021-06-22 12:20:44 -0500

    solved plus one

--- Python/66.plus-one.py
+++ Python/66.plus-one.py
@@ -8,7 +8,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         output = []
-        num = [str(int) for int in digits]
-        print(num)
+        num = ''
+        for digit in digits:
+            num += str(digit)
+        plus_one = int(num) + 1
+        for _ in str(plus_one):
+            output.append(_)
+        return output
 # @lc code=end
 
