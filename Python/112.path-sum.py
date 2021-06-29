a5762ffa6a698042ed19a0a2f538bbde6a01d2d1
Steve <stevekentlanier@gmail.com>
2021-06-29 12:57:57 -0500

    started firstMissingPositive

---
+++ Python/112.path-sum.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=112 lang=python3
+#
+# [112] Path Sum
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+    
+    def isLeaf(self, node):
+        return node.left is None and node.right is None
+    
+    def rootToLeafPaths(self, node, path):
+        if node is None:
+            return
+        
+        path.append
+        
+# @lc code=end
+
---
+++ Python/41.first-missing-positive.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=41 lang=python3
+#
+# [41] First Missing Positive
+#
+
+# @lc code=start
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        
+# @lc code=end
+
